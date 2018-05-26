9056765cf3e1f16107fd6fa8a9f966868cd48a9d
Andreas Fuchs <asf@boinkor.net>
2018-05-26 20:44:52 +0200

    Improve property test suite
    * Now tests the array encoding method too (only u64 for now)
    * Make clippy happy

--- tests/prop.rs
+++ tests/prop.rs
@@ -5,9 +5,8 @@ extern crate num;
 #[macro_use]
 extern crate proptest;
 
-use bit_vec::BitVec;
-use fibonacci_codec::EncodeOne;
 use fibonacci_codec::{fib_decode_u16, fib_decode_u32, fib_decode_u64, fib_decode_u8};
+use fibonacci_codec::{Encode, EncodeOne};
 
 use proptest::prelude::*;
 
@@ -28,9 +27,17 @@ proptest! {
             let decoded: Vec<u32> = fib_decode_u32(&bits).filter_map(|x| x.ok()).collect();
             prop_assert_eq!(decoded, vec![input as u32], "Decoding as u32");
         }
-        if input <= 0xffffffffffffffff {
-            let decoded: Vec<u64> = fib_decode_u64(&bits).filter_map(|x| x.ok()).collect();
-            prop_assert_eq!(decoded, vec![input as u64], "Decoding as u64");
-        }
+        let decoded: Vec<u64> = fib_decode_u64(&bits).filter_map(|x| x.ok()).collect();
+        prop_assert_eq!(decoded, vec![input as u64], "Decoding as u64");
+    }
+
+    #[test]
+    fn roundtrips_array(ref input in
+                        proptest::collection::vec(any::<u64>().prop_filter("Values must be >0".to_owned(),
+                                                                           |v| *v > 0),
+                                                  1..100)) {
+        let bits = input.clone().fib_encode().expect("Expected an Ok result");
+        let decoded: Vec<u64> = fib_decode_u64(bits).filter_map(|x| x.ok()).collect();
+        prop_assert_eq!(decoded, input.clone());
     }
 }
